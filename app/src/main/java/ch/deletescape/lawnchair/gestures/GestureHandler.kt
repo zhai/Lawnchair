000ddadd7a8682d4667b33bec3e9fe8bf013181b
zhai <masterzxg@gmail.com>
2018-11-20 15:15:28 +0800

    更改包名为 com.zhaisoft.mylauncher

--- app/src/main/java/ch/deletescape/lawnchair/gestures/GestureHandler.kt
+++
@@ -1,8 +0,0 @@
-package ch.deletescape.lawnchair.gestures
-
-import ch.deletescape.lawnchair.Launcher
-
-abstract class GestureHandler(val launcher: Launcher) {
-
-    abstract fun onGestureTrigger()
-}
